c76eda7fcbe4838b90204bfc9a227231f892dc28
Aashish Choudhary <aashish01official@gmail.com>
2021-09-06 13:26:18 +0530

    initital commit

---
+++ linkedList/designTwitter.java
@@ -0,0 +1,7 @@
+package linkedList;
+
+public class designTwitter {
+public static void main(String[] args) {
+	System.out.println("hello world");
+}
+}
---
+++ linkedList/lruCache.java
@@ -0,0 +1,7 @@
+package linkedList;
+
+public class lruCache {
+public static void main(String[] args) {
+	System.out.println("up and running");
+}
+}
